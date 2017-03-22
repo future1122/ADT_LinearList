7b9a9057e780f97c14cdc4910c7d22cc090cb5ff
future1122 <jcb626316770@vip.qq.com>
2017-03-22 18:27:42 +0800

    SingleList
    Signed-off-by: future1122 <jcb626316770@vip.qq.com>

--- ADT_LinearList/ADT_LinearList.cpp
+++ ADT_LinearList/ADT_LinearList.cpp
@@ -8,6 +8,14 @@ int main()
 {
 	SeqList<int> listA(20);
 	SingleList<int> listB;
+	for (int i = 0; i < 10; ++i) {
+		listA.Insert(i-1, i);
+	}
+	for (int i = 0; i < 5; ++i) {
+		listB.Insert(i - 1, i);
+	}
+	listA.Output(std::cout);
+	listB.Output(std::cout);
     return 0;
 }
 
